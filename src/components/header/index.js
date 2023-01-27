99f1ba4a11dc19975bc3c38d0647f288e5e83b25
clbutl <cannonlbutler@gmail.com>
2023-01-27 01:38:07 -0500

    mobile header hides after page change

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,6 +10,11 @@ const Header = () => {
     changeNav(!currentNav)
   }
 
+  const switchPage = () => {
+    changeNav(true)
+    console.log("change")
+  }
+
   document.title = 'Cannon Butler Portfolio'
   return (
     <header>
@@ -17,13 +22,13 @@ const Header = () => {
         <h1 className="name-header">Cannon Butler</h1>
       </div>
       <div className="header-links-div">
-        <Link className={currentNav ? 'header-links hide-nav' : 'header-links'} to='/'>
+        <Link onClick={switchPage} className={currentNav ? 'header-links hide-nav' : 'header-links'} to='/'>
           <h2>Home</h2>
         </Link>
-        <Link className={currentNav ? 'header-links hide-nav' : 'header-links'} to='/projects'>
+        <Link onClick={switchPage} className={currentNav ? 'header-links hide-nav' : 'header-links'} to='/projects'>
           <h2>Projects</h2>
         </Link>
-        <Link className={currentNav ? 'header-links hide-nav' : 'header-links'} to='/resume'>
+        <Link onClick={switchPage} className={currentNav ? 'header-links hide-nav' : 'header-links'} to='/resume'>
           <h2>Resume</h2>
         </Link>
       </div>
